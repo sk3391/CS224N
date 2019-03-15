3fc439133803c5e3e8293e1ee4acec7c8133071c
Simerjot Kaur <simerjotkaur@Simerjots-MacBook-Pro.local>
2019-03-15 18:08:09 -0400

    changed loss function to cross entropy

--- squad/train.py
+++ squad/train.py
@@ -117,7 +117,8 @@ def main(args):
                 #log_p1, log_p2 = model(cw_idxs, qw_idxs)
                 log_p1, log_p2 = model(cw_idxs, cc_idxs, qw_idxs, qc_idxs)
                 y1, y2 = y1.to(device), y2.to(device)
-                loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
+                #loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
+                loss = F.cross_entropy(log_p1, y1) + F.cross_entropy(log_p2, y2)
                 loss_val = loss.item()
 
                 # Backward
